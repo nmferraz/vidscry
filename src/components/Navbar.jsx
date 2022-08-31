5bb2d2140252ce6634899f6768df6014fae359ee
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-09-01 00:46:52 +0100

    Navbar only with the logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,23 @@
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
-  return (
-    <Stack
-      direction="row"
-      alignItems="center"
-      p={2}
-      sx={{
-        position: "sticky",
-        background: "#000",
-        top: 0,
-        justifyContent: "space-between",
-      }}
-    >
-      Navbar
-    </Stack>
-  );
-};
+import { logo } from "../utils/constants";
 
+const Navbar = () => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      position: "sticky",
+      background: "#000",
+      top: 0,
+      justifyContent: "space-between",
+    }}
+  >
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+  </Stack>
+);
 export default Navbar;
