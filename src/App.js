84a0fd1c129d41a320f4c4bd95bf2ee89102fd68
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-09-01 00:32:51 +0100

    fixing Box close tag

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ const App = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: "#000" }}>
       <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
     </Box>
-    <Routes>
-      <Route path="/" exact element={<Feed />} />
-      <Route path="/video/:id" element={<VideoDetail />} />
-      <Route path="/channel/:id" element={<ChannelDetail />} />
-      <Route path="/search/:searchTerm" element={<SearchFeed />} />
-    </Routes>
   </BrowserRouter>
 );
 
