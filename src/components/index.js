2fad54e3aa288d50d2c85dd06acef69bd7282e01
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-09-01 01:48:54 +0100

    VideoCard

--- src/components/index.js
+++ src/components/index.js
@@ -7,3 +7,4 @@ export { default as SearchBar } from "./SearchBar";
 export { default as Sidebar } from "./Sidebar";
 export { default as Videos } from "./Videos";
 export { default as Loader } from "./Loader";
+export { default as VideoCard } from "./VideoCard";
