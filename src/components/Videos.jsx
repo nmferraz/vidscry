fdf89271fc760b2353e38f3d0ce8d8571d3cd842
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-09-01 01:44:12 +0100

    fixing BASE_URL and console log videos

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
-const Videos = () => {
-  return (
-    <div>Videos</div>
-  )
-}
+const Videos = ({ videos }) => {
+  console.log(videos);
 
-export default Videos
+  return <div>Videos</div>;
+};
+
+export default Videos;
